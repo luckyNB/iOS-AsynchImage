f5733a7358e5f96ba80f466f1ea94c648aebe5d4
luckyNB <20150773@dbatu.ac.in>
2022-12-27 18:55:59 +0530

    learned asynch image

---
+++ SwiftUIAsynchImage/SwiftUIAsynchImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsynchImageApp.swift
+//  SwiftUIAsynchImage
+//
+//  Created by Laxman Bhosle on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsynchImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
